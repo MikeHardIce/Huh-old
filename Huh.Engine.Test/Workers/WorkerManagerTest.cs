abf90ec2db9b8dfd4ffbae54737a9f3920ae59fc
OgreBelly <>
2019-06-01 16:56:37 +0100

    added simple test for workers

--- Huh.Engine.Test/Workers/WorkerManagerTest.cs
+++ Huh.Engine.Test/Workers/WorkerManagerTest.cs
@@ -1,7 +1,10 @@
 
+using System.Threading;
 using Huh.Core.Steps;
 using Huh.Core.Tasks;
+using Huh.Engine.Workers;
 using Moq;
+using Shouldly;
 using Xunit;
 
 namespace Huh.Engine.Test.Workers 
@@ -11,12 +14,26 @@ namespace Huh.Engine.Test.Workers
         public WorkerManagerTest ()
         {
             
-
         }
 
         [Fact]
         public void TestStartStop ()
         {
+            var stepInfo = new Mock<IStepInformation>();
+            var task     = new Mock<ITask>();
+
+            var manager = new WorkerManager();
+
+            manager.StepManager.Register(stepInfo.Object);
+
+            manager.TaskManager.TaskCollection.Add(task.Object);
+
+            manager.Start();
+
+            //TODO: Check out alternatives to this
+            Thread.Sleep(500);
+
+            manager.TaskManager.TaskCollection.Empty.ShouldBeTrue();
             
         }
     }
